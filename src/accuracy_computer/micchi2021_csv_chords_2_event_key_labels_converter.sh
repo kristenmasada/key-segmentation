75a0ea489f4967d18d4b30e85775b2fa82c03ae8
Kristen Masada <km942412@ohio.edu>
2022-09-27 15:06:46 -0400

    Add several missing files + update .gitignore.

---
+++ src/accuracy_computer/micchi2021_csv_chords_2_event_key_labels_converter.sh
@@ -0,0 +1,2 @@
+#python3 micchi2021_csv_chords_2_event_key_labels_converter.py --txt_file_with_csv_filepaths 'in/micchi2021_train_csv_filepaths.txt' --input_type 'pitch_bass'
+python3 micchi2021_csv_chords_2_event_key_labels_converter.py --txt_file_with_csv_filepaths 'in/micchi2021_validation_csv_filepaths.txt' --input_type 'pitch_bass'
